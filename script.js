032b942e45b134adfb08f6b82fd1581d39fa5cfb
himalayan <79290770+mattyb5000@users.noreply.github.com>
2021-03-20 01:18:53 -0400

    Added prompt and confirm windows for length and character types.

--- script.js
+++ script.js
@@ -5,12 +5,23 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
+  
   passwordText.value = password;
 
 }
 
 // Add event listener to generate button
+function generatePassword() {
+  var passwordLength = prompt("What character length does your password need to be?");
+  var passwordLowercase = confirm("Do you want any lowercase letters?");
+  var passwordUppercase = confirm("Do you want any uppercase letters?");
+  var passwordNumeric = confirm("Do you want any numbers?");
+  var passwordSpecial = confirm("Do you want any special characters?");
+  console.log(passwordLowercase)
+  return "My password"
+}
+
+// this starts things off calling on function "writePassword"
 generateBtn.addEventListener("click", writePassword);
 
 
