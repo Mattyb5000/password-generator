e8f52993032ff910b61285fc0b4cb1ceb06784bb
himalayan <79290770+mattyb5000@users.noreply.github.com>
2021-03-20 19:29:57 -0400

    added passwordLenth prompt with alert if below acceptalbe length criteria.

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var chosenCount = generatePassword.passwordLength;
 
 // Write password to the #password input
 function writePassword() {
@@ -7,19 +8,40 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
   
   passwordText.value = password;
-
 }
-
+writePassword()
 // Add event listener to generate button
 function generatePassword() {
-  var passwordLength = prompt("What character length does your password need to be?");
-  var passwordLowercase = confirm("Do you want any lowercase letters?");
-  var passwordUppercase = confirm("Do you want any uppercase letters?");
-  var passwordNumeric = confirm("Do you want any numbers?");
-  var passwordSpecial = confirm("Do you want any special characters?");
-  console.log(passwordLowercase)
-  return "My password"
+  var passwordLength = prompt("What character length does your password need to be? (*At least 8 and no more than 128 characters.");
+  console.log(passwordLength)
+  if(passwordLength < 8) {
+    alert("Must be more than 8 characters.");
+  }
+
+
+  // var confirmLength = alert(passwordLength + " characters.")
+    // return("My password")
+    
+
+  //   console.log(passwordLength)
+  // var passwordLowercase = confirm("Do you want any lowercase letters?");
+  // var confirmLowercase = alert(passwordLowercase + " characters.")
+  //   console.log(passwordLowercase)
+  // var passwordUppercase = confirm("Do you want any uppercase letters?");
+
+  //   console.log(passwordUppercase)
+  // var passwordNumeric = confirm("Do you want any numbers?");
+
+  //   console.log(passwordNumeric)
+  // var passwordSpecial = confirm("Do you want any special characters?");
+
+  //   console.log(passwordSpecial)
+ 
+  
+  // return "My password"
 }
+
+// generatePassword()
 
 // this starts things off calling on function "writePassword"
 generateBtn.addEventListener("click", writePassword);
